70c0c8a5f81effd907d2a8481d106b75cc5ffe03
LordZex203 <naufalakbaremail012@gmail.com>
2021-11-04 01:28:10 +0000

    Get Icon Server Discord

---
+++ commands/Guild Info/getIconGuild.js
@@ -0,0 +1,26 @@
+const { Message, Client, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "geticondiscord",
+    aliases: ['geticon', 'gi'],
+    UserPerms: [""],
+    BotPerms: [""],
+    cooldown: 5,    
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+        const msgEmbed = new MessageEmbed()
+            .setAuthor('OWNZEX BOT', 'http://34.126.184.54/image/ppownzex.png')
+            .setColor('RANDOM')
+            .setDescription(`Icon Discord ${message.guild.name}`)        
+            .setFooter(`Request By ${message.author.tag}`)
+            .setImage(`${message.guild.iconURL({ size: 1024 })}`)
+            .setTimestamp()
+            .setTitle(`Get Icon Discord Server ${message.guild.name}`)                
+        message.reply({ embeds: [msgEmbed] });
+    },
+};
