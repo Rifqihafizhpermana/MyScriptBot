890d7ac96eca2d6cbb18f93f47d421ddd7d7051c
LordZex203 <naufalakbaremail012@gmail.com>
2021-11-04 00:40:25 +0000

    Update Sedikit

--- commands/User Info/getAvatarUser.js
+++ commands/User Info/getAvatarUser.js
@@ -13,20 +13,18 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, message, args) => {
+        if (!args[0]) return message.reply("**Please mention a member first!**")
+        const getTarget = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(m => m.displayName.toLowerCase() === args[0].toLocaleLowerCase()) || message.guild.members.cache.find(m => m.user.username.toLowerCase() === args[0].toLocaleLowerCase())
+        if (!getTarget) return message.reply("**The user you provided is not valid in this guild, try using User ID or User Name or try to mention the member!**")
 
         const msgEmbed = new MessageEmbed()
             .setAuthor('OWNZEX BOT', 'http://34.126.184.54/image/ppownzex.png')
             .setColor('RANDOM')
-            .setDescription()        
-            .setFields(
-                { name: '', value: '' },
-                { name: '', value: '' }
-            )
-            .setFooter()
-            .setImage()
-            .setThumbnail()
+            .setDescription(`Avatar ${getTarget.user.tag}`)        
+            .setFooter(`Request By ${message.author.tag}`)
+            .setImage(`${getTarget.user.avatar}`)
             .setTimestamp()
-            .setTitle()
+            .setTitle(`Get Avatar User ${getTarget.user.tag}`)
         message.reply({ embeds: [msgEmbed] });
 
     },
